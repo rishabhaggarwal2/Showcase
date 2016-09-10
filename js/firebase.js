a578baa064355aedf94e74c8f7782c9cdc2689c5
SlashDK <dkhurana@andrew.cmu.edu>
2016-09-10 16:55:19 -0400

    Starter firebase

---
+++ js/firebase.js
@@ -0,0 +1,7 @@
+var config = {
+    apiKey: "AIzaSyBb8PzIF3-Dz26htyuTHuGonXj5ZMO9-z8",
+    authDomain: "showcase-7719f.firebaseapp.com",
+    databaseURL: "https://showcase-7719f.firebaseio.com",
+    storageBucket: "",
+  };
+firebase.initializeApp(config);
